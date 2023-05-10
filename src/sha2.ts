e4c47222edeaa9a7055a092ee1d74f7b65cf3cdc
Sergey Ukustov <sergey@ukstv.me>
2023-05-10 14:51:05 +0300

    fix: Coerce to Uint8Array

--- src/sha2.ts
+++ src/sha2.ts
@@ -1,9 +1,10 @@
 import { createHash } from "crypto";
 import { SyncHasher } from "./sync-hasher.js";
+import { coerce } from "multiformats/bytes";
 
-export const sha256 = new SyncHasher<"sha2-256", 0x12>("sha2-256", 0x12, (bytes) => {
-  return createHash("sha256").update(bytes).digest();
+export const sha256 = new SyncHasher<"sha2-256", 0x12>("sha2-256", 0x12, (input) => {
+  return coerce(createHash("sha256").update(input).digest());
 });
-export const sha512 = new SyncHasher<"sha2-512", 0x13>("sha2-512", 0x13, (bytes) => {
-  return createHash("sha512").update(bytes).digest();
+export const sha512 = new SyncHasher<"sha2-512", 0x13>("sha2-512", 0x13, (input) => {
+  return coerce(createHash("sha512").update(input).digest());
 });
